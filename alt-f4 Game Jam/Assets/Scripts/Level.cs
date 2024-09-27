ee1e460889349fa9dd68f1175962fde0aa77bcbf
dashadowofcat <104553632+dashadowofcat@users.noreply.github.com>
2024-09-27 16:32:17 +0300

    added room system

---
+++ alt-f4 Game Jam/Assets/Scripts/Level.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public string Name;
+
+    public BoxCollider2D Bounds;
+
+    public GameObject Contents;
+
+    public Transform CameraAnchor;
+
+    public Level[] LevelsToDisable;
+
+    public bool PreloadLevel;
+
+
+    private void Awake()
+    {
+        Contents.SetActive(PreloadLevel);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (PreloadLevel) return;
+
+        OnEnter();
+    }
+
+    public void OnEnter()
+    {
+        foreach (Level level in LevelsToDisable)
+        {
+            level.DisableLevel();
+        }
+
+        EnableLevel();
+
+        FindObjectOfType<Camera>().transform.position = CameraAnchor.transform.position;
+    }
+
+    public void EnableLevel()
+    {
+        Contents.SetActive(true);
+    }
+
+    public void DisableLevel()
+    {
+        Contents.SetActive(false);
+    }
+}
